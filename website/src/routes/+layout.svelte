39adb123092d2ebba7f62111bd5904b11db0fd15
David Mynors <hello@davidmyno.rs>
2025-11-21 19:32:32 +0000

    Set title/favicon

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
+	import "../app.css";
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>album recs</title>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💿</text></svg>"
+	/>
 </svelte:head>
 
 {@render children?.()}
