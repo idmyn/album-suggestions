0ceeb3718be4d1f29662dcf4690c4598236abce3
David Mynors <hello@davidmyno.rs>
2026-03-06 19:38:43 +0000

    prep for railway deployment (#6)
    * prep for railway deployment
    
    * add db:migrate script to root
    
    * remove cloudflare cron trigger

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,13 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: "plaintext",
-			routes: {
-				include: ["/*"],
-				exclude: ["<all>"],
-			},
-		}),
+		adapter: adapter(),
 		experimental: {
 			remoteFunctions: true,
 		},
